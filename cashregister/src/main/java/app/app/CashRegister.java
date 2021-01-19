fa665af6f71b5b25c1234c6efe2c67a4fab86e96
Dylan Frye <dfryed@gmail.com>
2021-01-19 14:56:01 -0600

    first commit

---
+++ cashregister/src/main/java/app/app/CashRegister.java
@@ -0,0 +1,21 @@
+package app;
+
+public class CashRegister {
+    private Register register;
+    
+    public void show() {
+
+    }
+    public void put() {
+
+    }
+    public void take() {
+
+    }
+    public void change() {
+
+    }
+    public void quit() {
+
+    }
+}
---
+++ cashregister/src/main/java/app/models/Register.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Register {
+    private int ones;
+    private int twos;
+    private int fives;
+    private int tens;
+    private int twenties;
+}
